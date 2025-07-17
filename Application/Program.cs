c394c5ed786c1591996fa666f00cb983217606aa
xBiscuitz <me.mikedevil@gmail.com>
2025-07-17 19:47:16 +0800

    Sample

--- Application/Program.cs
+++ Application/Program.cs
@@ -11,7 +11,7 @@ using VG.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//// Add services to the container.
+//// Add services to the container...
 //builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 //    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
